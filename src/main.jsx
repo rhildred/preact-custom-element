93000be1b11000a03e97b8dd7a013303c69280f5
Rich Hildred <rhildred@gmail.com>
2025-04-06 10:04:11 -0400

    switch to pico for scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,33 @@
 import register from './index.js';
 //import register from "@rhildred/preact-custom-element";
+import styles from './main.scss?inline';
 
 function Scoped() {
-	return <h1>In Scoped custom Element</h1>;
+	return (
+		<>
+			<h1>In Scoped custom Element</h1>
+			<form>
+				<label>
+					question
+					<input placeholder="Ask me Anything" />
+				</label>
+				<label>
+					object
+					<input name="noun" placeholder="Enter a noun" />
+				</label>
+				<button
+					onClick={(evt) => {
+						alert('question');
+					}}
+				>
+					Ask
+				</button>
+			</form>
+		</>
+	);
 }
 
 const sheet = new CSSStyleSheet();
-sheet.replaceSync('h1 { color: blue; border: 2px dotted black;}');
+sheet.replaceSync(styles);
 
 register(Scoped, 'x-scoped', [], { shadow: true, adoptedStyleSheets: [sheet] });
